b9b28282ff805b1c64e74a39163bb094e0d8283b
Tejas20002 <tejashirani55@gmail.com>
2022-11-29 22:11:37 +0530

    [*] tutorial of gb is ready

--- restaurant.py
+++ restaurant.py
@@ -16,8 +16,11 @@ lst_qty = []
 dict_menu = {1: 'Dosa', 2: 'Pani Puri', 3: 'Maggi', 4: 'Ice Cream', 5: 'Coffee', 6: 'Half Tea'}
 dict_price = {1: 30, 2: 30, 3: 50, 4: 30, 5: 40, 6: 10}
 bill_print = ""
+
 pdf = FPDF('L', 'mm', 'A4')
 pdf.add_page()
+pdf.set_font('Arial', 'B', 16)
+
 print("Menu Teja Hotel")
 print("--------------------------------")
 for s in dict_menu:
@@ -60,23 +63,25 @@ while (1):
             continue
 
     elif (a == 'n' or a == 'N'):
-        bill_print = bill_print + 'Teja Hotel\nBILL:\n------------------------------------------------------------------------------------------------'
+        pdf.cell(40, 10, 'Teja Hotel', 0, 1)
+        pdf.cell(40, 10, 'BILL:', 0, 1)
+        pdf.cell(40, 10, '------------------------------------------------------------------------------------------------',0,1)
         total = 0
         length = len(lst_choice)
 
         for i in range(0, length):
             val = lst_choice[i]
-            bill_print = f"""{i+1} {dict_menu[val]}          Qty:- {lst_qty[i]}            Rs:- {dict_price[val]} per unit
-------------------------------------------------------------------------------------------------"""
+            bill_print = f"""{i+1} {dict_menu[val]}          Qty:- {lst_qty[i]}            Rs:- {dict_price[val]} per unit"""
+            pdf.cell(40,10, bill_print, 0, 1)
+            pdf.cell(40,10, "------------------------------------------------------------------------------------------------",0 ,1)
             total = total + (dict_price[val] * lst_qty[i])
-        bill_print = bill_print + f"Total = Rs. {total}"
-        bill_print = bill_print + "\n------------------------------------------------------------------------------------------------')"
+        bill_prints = f"Total = Rs. {total}"
+        pdf.cell(40, 10, bill_prints, 0, 1)
+        # pdf.cell(40,10, "------------------------------------------------------------------------------------------------",0 ,1)
         print('Thank you')
         # f = open(name+".pdf",'x')
         # f.write(bill_print)
         # f.close()
-        pdf.set_font('Arial', 'B', 16)
-        pdf.cell(40, 10, bill_print)
         pdf.output(name+'.pdf')
         break
 
