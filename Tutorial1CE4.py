0da64f3bcba58398644ff0d39184e8d62c6959be
Tejas20002 <tejashirani55@gmail.com>
2023-01-05 20:51:23 +0530

    [*] add coment

--- Tutorial1CE4.py
+++ Tutorial1CE4.py
@@ -1,12 +1,9 @@
 import pdfkit
 import pandas as pd
+# for windows users uncomment line
+# conf = pdfkit.configuration(wkhtmltopdf='PATH_OF_WKHTMLTOPDF_APP_FILE')
 data = pd.read_csv('./data.csv')
 rowcount  = 0
 for row in open("./data.csv"):
-    pdfkit.from_url(data.url[rowcount],data.name[rowcount]+".pdf")
+    pdfkit.from_url(data.url[rowcount],data.name[rowcount]+".pdf") # add the "configuration = config" for window users
     rowcount+=1
-
-# for i in range(rows):
-#     for j in range(columns):
-#         data.loc[i, columnHeaders[j]] = self.ui.tableWidget.item(i, j).text()
-# pdfkit.from_url('https://pyfpdf.readthedocs.io/en/latest/index.html#installation','yepe.pdf') 
